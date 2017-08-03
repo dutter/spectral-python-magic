4a9d5b8fd1fffbbf14987a740ed912d870aa6259
Daniel Utter <dutter@g.harvard.edu>
2017-08-03 16:06:56 -0400

    making skeleton files and dirs

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # spectral-python-magic
+
+Goal is to be able to process images in ./test-images, using references in ./test-references, as effectively as the matlab version in ./matlab-version
+ 
---
+++ spectral-python-magic.py
@@ -0,0 +1,18 @@
+import numpy as np
+import matplotlib
+matplotlib.use("MacOSX")
+import matplotlib.pyplot as plt
+
+def getCziImage(cziFile='image.czi')
+    pass
+
+def getReferences(refDir='references')
+    pass
+
+def splitImage(image)
+    pass
+
+def lsqnonneg(singleSlice)
+    pass
+
+
